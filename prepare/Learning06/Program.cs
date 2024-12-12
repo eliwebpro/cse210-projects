6a68db25644aed847ce3ac769ba69f0a69e6c5cf
eliwebpro <elieziopingo1989@gmail.com>
2024-12-11 22:37:14 -0700

    Shape

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Rectangle("Green", 2, 5));
+        shapes.Add(new Circle("Red", 3));
+        shapes.Add(new Square("Brown", 5));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape color: {shape.GetColor()}");
+            Console.WriteLine($"Shape area: {shape.GetArea()}");
+        }
+
     }
 }
---
+++ stubs.cs
@@ -0,0 +1,156 @@
+// goal parent class
+public abstract class Goal
+{
+    protected string _shortName;
+    protected string _description;
+    protected int _points;
+
+    public Goal(string name, string description, int points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+
+    public abstract void RecordEvent();
+    public abstract bool IsComplete();
+    public virtual string GetDetailsString()
+    {
+        return $"[{(IsComplete() ? 'X' : ' ')}] {_shortName} - {_description}";
+    }
+    public abstract string GetStringRepresentation();
+}
+
+// simple goal child class from goal parent class
+public class SimpleGoal : Goal
+{
+    private bool _isComplete;
+
+    public SimpleGoal(string name, string description, int points) : base(name, description, points)
+    {
+        _isComplete = false;
+    }
+
+    public override void RecordEvent()
+    {
+        _isComplete = true;
+    }
+
+    public override bool IsComplete()
+    {
+        return _isComplete;
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"SimpleGoal|{_shortName}|{_description}|{_points}|{_isComplete}";
+    }
+}
+
+//eternal goal child class from goal parent class
+public class EternalGoal : Goal
+{
+    public EternalGoal(string name, string description, int points) : base(name, description, points) { }
+
+    public override void RecordEvent()
+   
+    public override bool IsComplete()
+    {
+        return false; 
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"EternalGoal|{_shortName}|{_description}|{_points}";
+    }
+}
+// check list goal
+public class ChecklistGoal : Goal
+{
+    private int _amountCompleted;
+    private int _target;
+    private int _bonus;
+
+    public ChecklistGoal(string name, string description, int points, int target, int bonus) : base(name, description, points)
+    {
+        _amountCompleted = 0;
+        _target = target;
+        _bonus = bonus;
+    }
+
+    public override void RecordEvent()
+    {
+        _amountCompleted++;
+    }
+
+    public override bool IsComplete()
+    {
+        return _amountCompleted >= _target;
+    }
+
+    public override string GetDetailsString()
+    {
+        return $"[{(IsComplete() ? 'X' : ' ')}] {_shortName} - {_description} ({_amountCompleted}/{_target})";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"ChecklistGoal|{_shortName}|{_description}|{_points}|{_amountCompleted}|{_target}|{_bonus}";
+    }
+}
+
+// goal manager class
+public class GoalManager
+{
+    private List<Goal> _goals;
+    private int _score;
+
+    public GoalManager()
+    {
+        _goals = new List<Goal>();
+        _score = 0;
+    }
+
+    public void Start()
+ 
+
+    public void DisplayPlayerInfo()
+    {
+        Console.WriteLine($"Current Score: {_score}");
+    }
+
+    public void ListGoalNames()
+    {
+        for (int i = 0; i < _goals.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {_goals[i].GetDetailsString()}");
+        }
+    }
+
+    public void CreateGoal(Goal goal)
+    {
+        _goals.Add(goal);
+    }
+
+    public void RecordEvent(int goalIndex)
+    {
+        if (goalIndex >= 0 && goalIndex < _goals.Count)
+        {
+            int pointsEarned = _goals[goalIndex].RecordEvent();
+            _score += pointsEarned;
+            Console.WriteLine($"You earned {pointsEarned} points!");
+        }
+        else
+        {
+            Console.WriteLine("Invalid goal selection.");
+        }
+    }
+
+    public void SaveGoals(string filePath)
+    {
+        var lines = _goals.Select(goal => goal.GetStringRepresentation());
+        File.WriteAllLines(filePath, lines);
+    }
+
+    public void LoadGoals(string filePath)
+  
